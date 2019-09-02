14796f0844390ce51426c553ffd6b11ce396567b
piginzoo <piginzoo@gmail.com>
2019-09-02 16:52:01 +0800

    update

--- utils/evaluator.py
+++ utils/evaluator.py
@@ -66,6 +66,8 @@ def validate(sess,batch_num,batch_size, generator,f_score, f_geometry,input_imag
             score = scores[i]
             geometry = geometrys[i]
 
+            timer = [('restore', 0),('nms', 0)]
+
             boxes, timer = detect(score_map=score, geo_map=geometry, timer=timer)
             bbox_pred = boxes[:, :8]
 
@@ -74,11 +76,13 @@ def validate(sess,batch_num,batch_size, generator,f_score, f_geometry,input_imag
             recall_sum += metrics['recall']
             f1_sum += metrics['hmean']
 
-            logger.debug("这个批次的图片#%d的探测结果的精确度:%f,召回率:%f,F1:%f",
+            logger.debug("这个批次的图片#%d的探测结果的精确度:%f,召回率:%f,F1:%f;耗时:restore:%f,NMS:%f",
                          i,
                          metrics['precision'],
                          metrics['recall'],
-                         metrics['hmean'])
+                         metrics['hmean'],
+                         timer['restore'],
+                         timer['name'])
 
     precision_mean = precision_sum / batch_num * batch_size
     recall_mean = recall_sum / batch_num * batch_size
